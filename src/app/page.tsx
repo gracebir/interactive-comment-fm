b64c69fae5d9cf085ec1927915a7b22b9571c429
gracebir <grace.devolop@gmail.com>
2023-09-17 17:08:04 +0200

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Comment from "./_components/Comment";
+import data from '../util/data.json'
 
 export default function Home() {
   return (
-    <main className="w-full">
-      <div className="max-w-5xl mx-auto">
-        <Comment/>
+    <main className="w-full px-6 lg:px-0 bg-light-gray min-h-screen">
+      <div className="max-w-5xl mx-auto py-8 lg:py-20">
+        <Comment
+          username={data.comments[0].user.username}
+          avatar={data.comments[0].user.image.png}
+          score={data.comments[0].score}
+          content={data.comments[0].content}
+          createAt={data.comments[0].createdAt}
+        />
       </div>
     </main>
   )
