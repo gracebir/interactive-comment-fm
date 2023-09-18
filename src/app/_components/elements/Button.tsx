579adc96634776a8b6401374d7a3aa628683969c
gracebir <grace.devolop@gmail.com>
2023-09-18 12:32:26 +0200

    type submit

--- src/app/_components/elements/Button.tsx
+++ src/app/_components/elements/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text}:{text: string}) => {
   return (
-    <button className="bg-blue font-medium uppercase rounded-md hover:bg-grayish duration-300 px-5 lg:px-6 py-2 lg:py-3 text-off-white">
+    <button type='submit' className="bg-blue font-medium uppercase rounded-md hover:bg-grayish duration-300 px-5 lg:px-6 py-2 lg:py-3 text-off-white">
       {text}
     </button>
   )
