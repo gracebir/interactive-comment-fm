5531872a6b065152fe6554a4471bd2e9ed858c9e
gracebir <grace.devolop@gmail.com>
2023-09-18 12:30:08 +0200

    Custom button

--- src/app/_components/elements/Button.tsx
+++ src/app/_components/elements/Button.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({text}:{text: string}) => {
   return (
-    <button>
-      
+    <button className="bg-blue font-medium uppercase rounded-md hover:bg-grayish duration-300 px-5 lg:px-6 py-2 lg:py-3 text-off-white">
+      {text}
     </button>
   )
 }
