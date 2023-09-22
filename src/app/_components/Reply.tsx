5e40153a427e1d84adfe0cfefd4c4b8837c4a883
gracebir <grace.devolop@gmail.com>
2023-09-22 19:15:14 +0200

    reply comment refractor

--- src/app/_components/Reply.tsx
+++ src/app/_components/Reply.tsx
@@ -1,26 +1,73 @@
-import { ReplyResponseProps } from '@/util/type'
-import React from 'react'
-import Comment from './Comment'
+'use client'
+import { ReplyProps } from '@/util/type'
+import { lowerFirstChar } from '@/util/utils'
+import data from '@/util/data.json'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { FaReply, FaMinus, FaPlus, FaTrash } from "react-icons/fa"
+import { PiPencilSimpleFill } from "react-icons/pi"
+import UpdateButton from './elements/UpdateButton'
+import EditForm from './EditForm'
 
-const Reply = ({ replies }: ReplyResponseProps) => {
-
+const Reply = ({ score, user, content, createdAt, replyingTo, id }: ReplyProps) => {
+  const currentUser = data.currentUser.username
+  const [isEdit, setIsEdit] = useState(false)
+  const [scoreValue, setScoreValue] = useState(score)
   return (
-    <div className='flex flex-row gap-4 lg:gap-16 ml-0 lg:ml-16'>
-      <div className='border border-gray-300'/>
-      <div className=' flex flex-col gap-6'>
-        {replies.map((replie) => (
-          <Comment
-            key={replie.id}
-            createAt={replie.createdAt}
-            content={replie.content}
-            replyingTo={replie.replyingTo}
-            isReply={true}
-            username={replie.user.username}
-            avatar={replie.user.image.png}
-            score={replie.score} />
-        ))}
+    <div className='bg-off-white p-7 lg:p-12 rounded-md grid grid-cols-1 lg:grid-cols-9 gap-4 lg:gap-6'>
+      <div className='flex flex-col order-1 lg:order-2 gap-4 lg:col-span-8 col-span-1'>
+        <div className='flex justify-between items-center'>
+          <div className='flex items-center text-base lg:text-xl gap-3'>
+            <Image src={user.image.png} alt={user.username} width={40} height={40} />
+            <span className='font-bold'>{user.username}</span>
+            <span>{createdAt}</span>
+          </div>
+          {currentUser === user.username ? (
+            <div className='hidden lg:flex gap-6 items-center'>
+              <UpdateButton className="text-red hover:text-palered" text='Detele' Icon={FaTrash} onClick={() => {}} />
+              <UpdateButton className='text-blue hover:text-grayish' text='Edit' Icon={PiPencilSimpleFill} onClick={() => setIsEdit(!isEdit)} />
+            </div>
+          ) : (
+            <button className='hidden lg:flex items-center gap-2 text-blue font-medium text-base lg:text-lg'>
+              <FaReply size={15} />
+              Reply
+            </button>
+          )}
+        </div>
+        {isEdit ? (
+          <EditForm id={id} value={`@${replyingTo} ${lowerFirstChar(content)}`}/>
+        ) : (
+          <p className="text-grayish-blue text-base lg:text-[1.4rem] leading-7 lg:leading-8">
+            <span className='text-blue font-medium'>@{replyingTo}</span>&nbsp;{lowerFirstChar(content)}
+          </p>
+        )
+        }
+      </div >
+      <div className='flex justify-between items-start col-span-1 order-2 lg:order-1'>
+        <div
+          className='flex flex-row items-center px-6 lg:px-5 space-x-4 rounded-md space-y-0 lg:flex-col lg:space-x-0 lg:space-y-4 p-4 bg-very-light-very'>
+          <button onClick={() => setScoreValue(scoreValue + 1)} className='text-grayish hover:text-blue'><FaPlus /></button>
+          <span className='text-lg lg:text-xl font-medium text-blue'>{scoreValue}</span>
+          <button onClick={() => {
+            if (scoreValue > 0) {
+              setScoreValue(scoreValue - 1)
+            }
+          }} className='text-grayish text-xl hover:text-blue'><FaMinus /></button>
+        </div>
+        {currentUser === user.username ? (
+          <div className='flex lg:hidden gap-4 items-center'>
+            <UpdateButton className="text-red hover:text-palered" text='Detele' Icon={FaTrash} onClick={() => { }} />
+            <UpdateButton className='text-blue hover:text-grayish' text='Edit' Icon={PiPencilSimpleFill} onClick={() => { }} />
+          </div>
+        ) : (
+          <button className='flex lg:hidden items-center gap-2 text-blue font-medium text-base lg:text-lg'>
+            <FaReply size={15} />
+            Reply
+          </button>
+        )}
+
       </div>
-    </div>
+    </div >
   )
 }
 
