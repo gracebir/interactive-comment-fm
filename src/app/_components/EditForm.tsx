94d96eb7a2a970f15ff75c94804f321303bed2b7
gracebir <grace.devolop@gmail.com>
2023-09-23 09:15:16 +0200

    edit function reply comment

--- src/app/_components/EditForm.tsx
+++ src/app/_components/EditForm.tsx
@@ -1,19 +1,28 @@
-import { EditFormProp, FormProps } from '@/util/type'
-import React from 'react'
-import data from '../../util/data.json'
+'use client'
+import { DataType, EditFormProp } from '@/util/type'
+import React, { useContext } from 'react'
 import TextField from './elements/TextField'
 import { useFormik } from 'formik'
 import Button from './elements/Button'
+import { updateReply } from '@/util/utils'
+import { AppContext } from '../_context/DataProvider'
 
 
-const EditForm = ({value, id}: EditFormProp) => {
+const EditForm = ({value, id, setIsOpen}: EditFormProp) => {
+    const {setDatas, datas} = useContext(AppContext)
     const { handleChange, errors, handleBlur, values, handleSubmit } = useFormik({
         initialValues: {
             comment: value ? value : ''
         },
         validateOnBlur: true,
         onSubmit: (value) => {
-           
+           const newsComment = updateReply(datas.comments, value.comment, id)
+           console.log(newsComment)
+           setDatas({
+            currentUser: datas.currentUser,
+            comments: newsComment
+           } as DataType)
+           setIsOpen(false)
         }
     })
   return (
