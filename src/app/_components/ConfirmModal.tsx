b59d470aee7edb3c19aae8c5b59bf057ec9e81a5
gracebir <grace.devolop@gmail.com>
2023-09-25 10:52:28 +0200

    handleckick props added

--- src/app/_components/ConfirmModal.tsx
+++ src/app/_components/ConfirmModal.tsx
@@ -1,18 +1,9 @@
 'use client'
-import { DataType, comfirmProps } from '@/util/type'
-import React, { useContext } from 'react'
-import { AppContext } from '../_context/DataProvider'
-import { deleteReply } from '@/util/utils'
+import { comfirmProps } from '@/util/type'
+import React from 'react'
 
-const ConfirmModal = ({id, setModalOpen}:comfirmProps) => {
-    const {datas, setDatas} = useContext(AppContext)
-    const onDelete = () => {
-        const newComment = deleteReply(datas.comments, id)
-        setDatas({
-          currentUser: datas.currentUser,
-          comments: newComment
-        } as DataType)
-      }
+const ConfirmModal = ({setModalOpen, handleClick}:comfirmProps) => {
+
   return (
     <div className="fixed right-0 inset-0 flex items-center left-0 w-full bg-opacity-50 px-6 lg:px-0 z-30 bg-black h-screen">
       <div className="bg-off-white mx-auto rounded-md max-w-md px-6 lg:px-8 py-6 flex-1 flex flex-col gap-4 lg:gap-5">
@@ -23,7 +14,7 @@ const ConfirmModal = ({id, setModalOpen}:comfirmProps) => {
         </p>
         <div className='grid grid-cols-2 gap-2 lg:gap-4 text-white'>
             <button onClick={()=> setModalOpen(false)} className="uppercase rounded-md w-full py-2 lg:py-3 bg-grayish-blue font-medium">no,cancel</button>
-            <button onClick={onDelete} className="uppercase rounded-md w-full py-2 lg:py-3 bg-red font-medium">delete</button>
+            <button onClick={handleClick} className="uppercase rounded-md w-full py-2 lg:py-3 bg-red font-medium">delete</button>
         </div>
       </div>
     </div>
