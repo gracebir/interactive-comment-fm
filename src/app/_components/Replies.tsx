96f27da9f60278ac6a6d8ab2d0c183f888a24b2d
gracebir <grace.devolop@gmail.com>
2023-09-27 12:24:15 +0200

    passed reply form idcoment

--- src/app/_components/Replies.tsx
+++ src/app/_components/Replies.tsx
@@ -2,14 +2,15 @@ import { ReplyResponseProps } from '@/util/type'
 import React from 'react'
 import Reply from './Reply'
 
-const Replies = ({ replies }: ReplyResponseProps) => {
+const Replies = ({ replies, idComment }: ReplyResponseProps) => {
   return (
     <div className='flex flex-row gap-4 lg:gap-16 ml-0 lg:ml-16'>
       <div className='border border-gray-300'/>
-      <div className=' flex flex-col gap-6'>
+      <div className=' flex flex-col flex-1 gap-6'>
         {replies.map((replie) => (
           <Reply
             key={replie.id}
+            idcomment={idComment}
             id={replie.id}
             user={replie.user}
             createdAt={replie.createdAt}
