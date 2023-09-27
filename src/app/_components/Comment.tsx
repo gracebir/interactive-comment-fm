98f82767c4cf2db1e57c035a97cc98d3283c9f91
gracebir <grace.devolop@gmail.com>
2023-09-27 12:22:58 +0200

    pass comment id into prop

--- src/app/_components/Comment.tsx
+++ src/app/_components/Comment.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { DataType, ReplyProps, commentProps } from '@/util/type'
+import { DataType, commentProps } from '@/util/type'
 import Image from 'next/image'
 import { PiPencilSimpleFill } from "react-icons/pi"
 import React, { useContext, useState } from 'react'
@@ -35,25 +35,6 @@ const Comment = ({ username, avatar, createAt, score, content, id }: commentProp
     setIsEditForm(false)
   }
 
-  // const addReply = (content: string, id: number) => {
-  //   const index = datas.comments.findIndex(item => item.id === id)
-  //   const idReply = (Math.floor(Math.random() * (1000 - 20 + 1)) + 20)
-  //   const newReply:ReplyProps = {
-  //     id: idReply,
-  //     content: content,
-  //     createdAt: "now",
-  //     replyingTo: datas.comments[index].user.username,
-  //     score: 0,
-  //     user: datas.currentUser,
-  //   }
-  //   if(datas.comments[index].replies){
-  //     setDatas({
-  //       ...datas,
-
-  //     })
-  //   }
-  //   console.log(newReply)
-  // }
   return (
     <div className='flex flex-col gap-4 lg:gap-6 duration-300'>
       <div className='bg-off-white p-7 lg:p-12 rounded-md grid grid-cols-1 lg:grid-cols-9 gap-4 lg:gap-6'>
@@ -81,9 +62,8 @@ const Comment = ({ username, avatar, createAt, score, content, id }: commentProp
           ) : (
             <p className="text-grayish-blue text-base lg:text-[1.4rem] leading-7 lg:leading-8">{content}</p>
           )}
-
         </div>
-        <div className='flex justify-between col-span-1 order-2 lg:order-1'>
+        <div className='flex justify-between items-start col-span-1 order-2 lg:order-1'>
           <div
             className='flex flex-row items-center px-6 lg:px-5 space-x-4 rounded-md space-y-0 lg:flex-col lg:space-x-0 lg:space-y-4 p-4 bg-very-light-very'>
             <button onClick={() => setScoreValue(scoreValue + 1)} className='text-grayish hover:text-blue'><FaPlus /></button>
--- src/app/_components/Comments.tsx
+++ src/app/_components/Comments.tsx
@@ -3,19 +3,19 @@ import React, { useContext, useEffect } from 'react'
 import Comment from './Comment'
 import Replies from './Replies'
 import Form from './Form'
-import { AppContext} from '../_context/DataProvider'
+import { AppContext } from '../_context/DataProvider'
 import { CommentsType } from '@/util/type'
 
-const Comments = ({data}: CommentsType) => {
-    const {datas, setDatas} = useContext(AppContext)
-    useEffect(()=>{
-      if(data){
-        setDatas(data)
-      }
-    },[])
-    
+const Comments = ({ data }: CommentsType) => {
+  const { datas, setDatas } = useContext(AppContext)
+  useEffect(() => {
+    if (data) {
+      setDatas(data)
+    }
+  }, [])
+
   return (
-     <div className="max-w-5xl mx-auto py-8 lg:py-20 flex flex-col gap-6">
+    <div className="max-w-5xl mx-auto py-8 lg:py-20 flex flex-col gap-6">
       {datas.comments ? (
         datas.comments.map((comment) => (
           <div key={comment.id} className="flex flex-col gap-6">
@@ -27,19 +27,19 @@ const Comments = ({data}: CommentsType) => {
               content={comment.content}
               createAt={comment.createdAt}
             />
-            {comment.replies.length ? <Replies replies={comment.replies}  />: null}
+            {comment.replies.length ? <Replies idComment={comment.id} replies={comment.replies} /> : null}
           </div>
         ))
-      ): (
+      ) : (
         <div className='h-[50vh] w-full flex items-center'>
           <p className='text-center mx-auto text-lg lg:text-lg font-medium'>Loading...</p>
         </div>
       )}
-        
-        <div className=" mt-6">
-          <Form isAdd={true} />
-        </div>
+
+      <div className=" mt-6">
+        <Form isAdd={true} />
       </div>
+    </div>
   )
 }
 
