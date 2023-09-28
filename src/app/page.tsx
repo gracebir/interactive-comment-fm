17301dcb2b6fcffe793babb563a7973a786ec1eb
gracebir <grace.devolop@gmail.com>
2023-09-28 09:06:35 +0200

    remove host

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 import { DataType } from '@/util/type';
+import data from '@/util/data.json'
 import Comments from './_components/Comments'
 
-async function fetchData() {
-  const response = await fetch(`http://127.0.0.1/api/comment`);
-  const data:DataType = await response.json()
-  return data
-}
-
 export default async function Home() {
-  const data = await fetchData()
   return (
     <main className="w-full px-6 lg:px-0 bg-light-gray min-h-screen">
-      <Comments data={data}/>
+      <Comments data={data as DataType}/>
     </main>
   )
 }
