34fb3630096df4e51e80695a67b72dd418bbed44
gracebir <grace.devolop@gmail.com>
2023-09-28 09:10:56 +0200

    get rid of the routes

--- src/app/api/comment/route.ts
+++
@@ -1,9 +0,0 @@
-import data from '@/util/data.json'
-export async function GET(request: Request){
-    const comment = data
-    return new Response(JSON.stringify(comment), {
-        headers: {
-            "Content-Type":"application/json"
-        }
-    })
-}
